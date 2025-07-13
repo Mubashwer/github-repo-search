0d063ded4fc11ec929cb4ed144ca63447c3ca2ab
Mubashwer Salman Khurshid <mubashwersk@gmail.com>
2025-07-14 01:27:43 +1000

    chore: rename branch from master to main
    - Rename local branch from master to main
    - Update semantic-release configuration to use main branch
    - Aligns with modern Git best practices and GitHub defaults

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: ["master"],
+  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
